a774a0246d6ce677b079ce843f5001a73fb75bb9
Raushan Kumar <raushankcse@gmail.com>
2025-09-14 19:54:07 +0530

    code

---
+++ DsaInJava/src/Day1/Age.java
@@ -0,0 +1,16 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Age {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int age = sc.nextInt();
+    if(age>=18){
+      System.out.println("Eligible to vote");
+    }else{
+      System.out.println("Not eligible to vote");
+    }
+  }
+
+}
---
+++ DsaInJava/src/Day1/Grade.java
@@ -0,0 +1,21 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Grade {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int marks = sc.nextInt();
+    if(marks>75){
+      System.out.println("A");
+    }else if(marks>=65 && marks<75){
+      System.out.println("B");
+    }else if(marks>=55 && marks<65){
+      System.out.println("C");
+    }else if(marks>=45&& marks<55){
+      System.out.println("D");
+    }else{
+      System.out.println("Fail");
+    }
+  }
+}
