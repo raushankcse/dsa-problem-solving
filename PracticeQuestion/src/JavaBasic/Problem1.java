328337f005629ec1510af223d63e69e7ebebedfd
Raushan Kumar <raushankcse@gmail.com>
2025-09-26 10:16:59 +0530

    practice

---
+++ PracticeQuestion/src/JavaBasic/Problem1.java
@@ -0,0 +1,27 @@
+package JavaBasic;
+
+import java.util.ArrayList;
+
+// reverse string
+public class Problem1 {
+  public static void main(String[] args) {
+    StringBuilder  s = new StringBuilder();  
+    s.append("hiiie")  ;
+    s.reverse();
+    String p = s.toString();
+    System.out.println(p);
+
+    int a[] = new int[10];
+    int b = a.length;
+
+
+
+
+
+
+
+
+
+
+  }
+}
