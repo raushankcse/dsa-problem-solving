21962eda44cf263a9cb56d2c25f3ee6882b69a92
Raushan Kumar <raushankcse@gmail.com>
2025-11-02 07:53:13 +0530

    competitive

--- DsaInJava/src/Day14/Question1.java
+++ DsaInJava/src/Day14/Question1.java
@@ -1,7 +1,24 @@
 package Day14;
 
+import java.util.Scanner;
+
 public class Question1 {
   public static void main(String[] args) {
-    
+    int[][] arr = new int[2][2];
+    Scanner sc = new Scanner(System.in);
+    System.out.println(arr);
+    for(int i=0;i<arr.length;i++){
+      for(int j=0;j<arr[i].length;j++){
+        arr[i][j] = sc.nextInt();
+      }
+    }
+
+
+    for(int i=0;i<arr.length;i++){
+      for(int j=0;j<arr[i].length;j++){
+        System.out.print(arr[i][j]+" ");
+      }
+      System.out.println();
+    }
   }
 }
