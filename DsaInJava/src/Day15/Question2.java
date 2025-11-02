8d6aa5e895d4f682fc609845d9d6d98318b55ceb
Raushan Kumar <raushankcse@gmail.com>
2025-11-02 13:56:42 +0530

    transpose

---
+++ DsaInJava/src/Day15/Question2.java
@@ -0,0 +1,28 @@
+package Day15;
+
+public class Question2 {
+  public static void main(String[] args) {
+    int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+  
+    transpose(arr);
+
+    for(int i=0;i<arr.length;i++){
+      for(int j=0;j<arr[0].length;j++){
+        System.out.print(arr[i][j]+" ");
+      }
+      System.out.println();
+    }
+    
+  }
+
+  public static void transpose(int[][] arr){
+    for(int i=0;i<arr.length;i++){
+      for(int j=i+1;j<arr.length;j++){
+        int temp = arr[i][j];
+        arr[i][j]=arr[j][i];
+        arr[j][i]=temp;
+      }
+    }
+  }
+
+}
