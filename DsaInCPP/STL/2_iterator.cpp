265453310726d0cb17bea9c244f7a64a57267846
Raushan Kumar <raushankcse@gmail.com>
2025-11-12 19:39:00 +0530

    iterator

---
+++ DsaInCPP/STL/2_iterator.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+
+  vector<int> arr = {1,2,3,4,5,6,8};
+  for(vector<int>::iterator it = arr.begin(); it!=arr.end();it++){
+    cout<<*it<<" ";
+  }
+
+
+
+  return 0;
+}
